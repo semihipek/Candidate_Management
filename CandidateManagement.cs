1e950f498f532e46bd7318aa0f4ebb3ade665e02
Semih İpek <semihipek247@gmail.com>
2023-07-19 14:59:19 +0300

    First Commit

---
+++ CandidateManagement.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace tasks_sem
+{
+
+    public class Candidate
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+
+}
+
